9d9358bad257ab82d8d590b84da5e5c4ea3491ce
Jonathan Eyler-Werve <jonathan@eylerwerve.com>
2016-01-27 19:40:50 -0600

    refactor tweetTriggerTextFilter

--- tweet.js
+++ tweet.js
@@ -11,18 +11,22 @@ window.mute.tweetTriggerTextFilter = function (triggerWords) {
     var tweetContent = $(tweet).find(".tweet-text").text().toLowerCase();
 
     $.each(triggerWords, function(index, word){
-      var regex = new RegExp(word);
+      var regex = new RegExp(word.toLowerCase());
       var match = regex.test(tweetContent);
 
       if (match) {
-        console.log("MATCHED", word, "WITH:", tweetContent);
-        $(tweet).css("background-color", "#E6F8E0");
-        $(tweet).css("border-color", "#D9E6D4");
-        $(tweet).css("color", "#B8CDB9");
-        $(tweet).find("a").css("color", "#A6B6A7")
-        $(tweet).find("strong, b, .tweet-text").css("color", "#B8CDB9")
-        window.mute.imageHide(tweet);
+        window.mute.tweetTriggerTextFilter.hideStuff(tweet, tweetContent)
         }
     });
   });
 };
+
+
+window.mute.tweetTriggerTextFilter.hideStuff = function (tweet, tweetContent) {
+  $(tweet).css("background-color", "#E6F8E0");
+  $(tweet).css("border-color", "#D9E6D4");
+  $(tweet).css("color", "#B8CDB9");
+  $(tweet).find("a").css("color", "#A6B6A7")
+  $(tweet).find("strong, b, .tweet-text").css("color", "#B8CDB9")
+  window.mute.imageHide(tweet);
+}
